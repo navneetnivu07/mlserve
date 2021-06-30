62062401c313358e4a7e80540c23420ec19a5abe
Navaneeth Malingan <navneetnivu07@gmail.com>
2021-06-30 12:34:40 +0530

    save model

--- README.md
+++ README.md
@@ -9,3 +9,13 @@ conda activate deploy
 pip install Flask
 
 ```
+
+```python
+
+import pickle
+ 
+# Save the trained model as a pickle string.
+filename = 'finalized_model.pkl'
+pickle.dump(model, open(filename, 'wb'))
+
+```
--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask import request
+import pickle
 
 app = Flask(__name__)
+filename = "finalized_model.sav"
+loaded_model = pickle.load(open(filename, 'rb'))
 
 @app.route("/")
 def hello_world():
@@ -15,6 +18,25 @@ def house():
     w2 = 4
     model = (int(area) * w1) + (int(rooms) * w2)
     return str(model)
+
+@app.route("/data/<section>")
+def data(section):
+    section = request.view_args['section']
+    return str(section)
+
+@app.route("/house-post", methods=['POST'])
+def house_post():
+    area = request.form.get('area')
+    return str(area)
+
+@app.route("/iris", methods=['POST'])
+def iris():
+    sl = request.form.get('sl')
+    sw = request.form.get('sw')
+    pl = request.form.get('pl')
+    pw = request.form.get('pw')
+    res = loaded_model.predict([[sl, sw, pl, pw]])
+    return str(res)
 
 if __name__ == '__main__':
 #    app.run(host='0.0.0.0', port=5000, debug=True)
